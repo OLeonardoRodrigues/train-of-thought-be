3c70bde6719776051d6c7ada27cc15a6ab954125
Rodrigo Santos <rodrigo.santos@qflash.com.br>
2021-08-18 13:03:42 -0300

    Adding cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 var express = require("express");
+var cors = require('cors');
 var app = express();
-const http = require("http");
 
+app.use(cors());
 
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+app.listen(3000, () => {
+  console.log("app running");
 });
 
-server.listen(port, hostname, () => {
-  console.log("Matheus gay");
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/%27);
+
+  res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://iflbmakaddmjmnngaemgfggeaoplipik');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("asd");
 });
